fda1942b7e889e31f65c5d30a4f71f12b7def9c3
prdaxca <100952967+FORTY71@users.noreply.github.com>
2026-08-31 07:36:56 +0900

    Update connect.py

--- api/connect.py
+++ api/connect.py
@@ -1,34 +1,9 @@
-import json
+from http.server import BaseHTTPRequestHandler
 
 
-def handler(request):
-    # Ambil parameter dari query
-    user_key = request.query.get("user_key", "")
-    serial = request.query.get("serial", "")
-
-    if user_key == "caca":
-        return {
-            "statusCode": 200,
-            "headers": {
-                "Content-Type": "application/json"
-            },
-            "body": json.dumps({
-                "status": "OK",
-                "message": "Key diterima",
-                "data": {
-                    "user_key": user_key,
-                    "serial": serial
-                }
-            })
-        }
-
-    return {
-        "statusCode": 401,
-        "headers": {
-            "Content-Type": "application/json"
-        },
-        "body": json.dumps({
-            "status": "ERROR",
-            "message": "Invalid key"
-        })
-    }
+class handler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        self.send_response(200)
+        self.send_header("Content-Type", "text/plain")
+        self.end_headers()
+        self.wfile.write(b"PYTHON OK")
